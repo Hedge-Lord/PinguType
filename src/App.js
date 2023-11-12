f82e30ce1fae40f81078799a10d2b04504005dfa
andhongrl <klutzjammin@gmail.com>
2023-11-11 19:27:16 -0800

    added button to react (not working but it is there), to fix, need to add react-dom router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { useState } from 'react';
-import './App.css';
 
-function App() {
+export default function Home() {
   const [timerStarted, setTimerStarted] = useState(false);
-
   function toggleMenu(x) {
-    let element, e2, e3;
-
+    var element; var e2; var e3;
     switch (x) {
-      case 0:
+      case 0: 
         element = document.getElementById("puncy");
         element.classList.toggle("punctuation-clicked");
         return;
@@ -18,98 +15,83 @@ function App() {
         return;
       case 2:
         element = document.getElementById("timey30");
-        element.classList.toggle("time30-clicked");
-        e2 = document.getElementById("timey15");
-        e3 = document.getElementById("timey60");
+        element.classList.toggle("time30-clicked")
+        var e2 = document.getElementById("timey15");
+        var e3 = document.getElementById("timey60");
         if (e2.className !== "time15") {
           e2.classList.toggle("time-clicked");
-        }
-        if (e3.className !== "time60") {
-          e3.classList.toggle("time-clicked");
+        } if (e3.className !== "time60") {
+          e3.classList.toggle("time-clicked")
         }
         return;
       case 3:
         element = document.getElementById("timey15");
-        element.classList.toggle("time-clicked");
+        element.classList.toggle("time-clicked")
         e2 = document.getElementById("timey30");
         e3 = document.getElementById("timey60");
-        if (e2.className === "time30") {
+        if (e2.className == "time30") {
           e2.classList.toggle("time30-clicked");
-        }
-        if (e3.className !== "time60") {
-          e3.classList.toggle("time-clicked");
+        } if (e3.className !== "time60") {
+          e3.classList.toggle("time-clicked")
         }
         return;
       case 4:
         element = document.getElementById("timey60");
-        element.classList.toggle("time-clicked");
+        element.classList.toggle("time-clicked")
         e2 = document.getElementById("timey30");
         e3 = document.getElementById("timey15");
-        if (e2.className === "time30") {
+        if (e2.className == "time30") {
           e2.classList.toggle("time30-clicked");
+        } if (e3.className !== "time15") {
+          e3.classList.toggle("time-clicked")
         }
-        if (e3.className !== "time15") {
-          e3.classList.toggle("time-clicked");
-        }
-        return;
-      default:
         return;
     }
-  }
-
+}
   function startTimer() {
-    let countDown = 0;
-    const endTime = 5;
-    const timerDisplay = document.getElementById("time");
-    const update = setInterval(function () {
-      const now = countDown++;
-      timerDisplay.innerHTML = "Time elapsed: " + countDown;
+    //make endTime into a parameter that changes based on the buttons
+  var countDown = 0;
+  var endTime = 5;
+  var timerDisplay = document.getElementById("time");
+  var update = setInterval(function () {
+      var now = countDown++;
+      timerDisplay.innerHTML = "Time: " + countDown;
+
 
       if (countDown > endTime) {
-        clearInterval(update);
-        timerDisplay.innerHTML = "Time's up!!!";
+          clearInterval(update);
+          timerDisplay.innerHTML = "Time's up!!!";
       }
-    }, 1000);
+  }, 1000);
   }
 
-  function startTimerOnce() {
-    if (!timerStarted) {
-      startTimer();
-      setTimerStarted(true);
+
+  function startTimerOnce(){
+    if (!timerStarted)
+    {
+        startTimer();
+        setTimerStarted(true);
     }
   }
 
   return (
     <div>
-      <meta charSet="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title> PinguType </title>
-      <link rel="stylesheet" href="../styles/style.css" />
-      <link rel="shortcut icon" href="../assets/favicon.ico" type="image/x-icon" />
-      <header>
-        <img src="../assets/pingutype.png" id="logo" alt="PinguType Logo" />
+      <header style={{ paddingLeft: '89px' }}>
+        <img src = "./pingutype.png" id="logo" alt="PinguType Logo" />
         <div className="title">
-          <p>pingutype</p>
+            pingutype
           <p className="subtext">a typing tool for penguins.</p>
         </div>
+        <div>
+          <button>Sign-In</button>
+        </div>
       </header>
-      <br />
       <div className="options">
-        <button id="puncy" className="punctuation" onClick={() => toggleMenu(0)}>
-          Punctuation
-        </button>
-        <button id="numby" className="numbers" onClick={() => toggleMenu(1)}>
-          Numbers
-        </button>
-        <button id="timey15" className="time15" onClick={() => toggleMenu(3)}>
-          15s
-        </button>
-        <button id="timey30" className="time30" onClick={() => toggleMenu(2)}>
-          30s
-        </button>
-        <button id="timey60" className="time60" onClick={() => toggleMenu(4)}>
-          60s
-        </button>
+        <button id="puncy" className="punctuation" onClick={() => toggleMenu(0)}> Punctuation </button>
+        <button class = "center" id="numby" className="numbers" onClick={() => toggleMenu(1)}> Numbers </button>
+        <button id="timey15" className="time15" onClick={() => toggleMenu(3)}> 15s </button>
+        <button id="timey30" className="time30" onClick={() => toggleMenu(2)}> 30s </button>
+        <button id="timey60" className="time60" onClick={() => toggleMenu(4)}> 60s</button>
       </div>
       <div id="typing-test">
         <label htmlFor="typing-area">
@@ -118,15 +100,52 @@ function App() {
         <div id="timerDisplay">
           <h3 id="time">Start typing to start the timer</h3>
         </div>
-        <textarea
-          id="typing-area"
-          rows={17}
-          cols={120}
-          onChange={startTimerOnce}
-        />
+        <textarea id="typing-area" rows="17" cols="120" onInput={() => startTimerOnce()}></textarea>
       </div>
     </div>
   );
 }
 
-export default App;
+function SignIn() {
+  return (
+    <html lang="en">
+    <head>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>PinguType</title>
+      <link rel="stylesheet" href="../styles/style.css" />
+      <link rel="shortcut icon" href="../assets/favicon.ico" type="image/x-icon" />
+    </head>
+    <body>
+      <header>
+        <img src="./pingutype.png" id="logo" alt="PinguType Logo" />
+        <div className="title">
+          <a href="index.html" style={{ textDecoration: 'none', color: 'black' }}>
+            pingutype
+          </a>
+          <p className="subtext">a typing tool for penguins.</p>
+        </div>
+      </header>
+      <h2 className="description-signup" style={{ marginBottom: 0 }}>
+        login or sign-up below
+      </h2>
+      <p className="username-spot">
+        <span style={{ fontSize: '18px' }}>
+          Username:
+          <textarea className="username-area" rows="1" cols="40" />
+        </span>
+      </p>
+      <p className="password-spot">
+        <span style={{ fontSize: '18px' }}>
+          Password:
+          <textarea className="password-area" rows="1" cols="40" />
+        </span>
+      </p>
+      <div className="signupAndLogin">
+        <button className="signup">Sign-up</button>
+        <button className="login">Login</button>
+      </div>
+    </body>
+  </html>
+  );
+}
