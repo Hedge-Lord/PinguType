31cd3cca88d6d7c264a3a12df5a2ae8dc97cec7a
andhongrl <klutzjammiN@gmail.com>
2023-11-11 21:00:06 -0800

    Added functionality to second buttons (now sets how long the timer will go on for) and also used states in startTimer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-
+import "./App.css"
 export default function Home() {
   const [timerStarted, setTimerStarted] = useState(false);
+  const [endTime, setEndTime] = useState(30);
   function toggleMenu(x) {
     var element; var e2; var e3;
     switch (x) {
@@ -47,37 +48,35 @@ export default function Home() {
         }
         return;
     }
-}
+  }  
   function startTimer() {
-    //make endTime into a parameter that changes based on the buttons
-  var countDown = 0;
-  var endTime = 5;
-  var timerDisplay = document.getElementById("time");
-  var update = setInterval(function () {
-      var now = countDown++;
-      timerDisplay.innerHTML = "Time: " + countDown;
-
-
-      if (countDown > endTime) {
+    if (!timerStarted)
+    {
+      setTimerStarted(true);
+      var countDown = 0;
+      var timerDisplay = document.getElementById("time");
+      var update = setInterval(function () {
+        var now = countDown++;
+        timerDisplay.innerHTML = "Time: " + countDown;
+  
+  
+        if (countDown > endTime) {
           clearInterval(update);
-          timerDisplay.innerHTML = "Time's up!!!";
-      }
-  }, 1000);
+            timerDisplay.innerHTML = "Time's up!!!";
+        }
+      }, 1000);
+    }
   }
 
 
-  function startTimerOnce(){
-    if (!timerStarted)
-    {
-        startTimer();
-        setTimerStarted(true);
-    }
+  function setButtonTime(endTime) {
+    setEndTime(endTime);
   }
 
   return (
     <div>
       <header style={{ paddingLeft: '89px' }}>
-        <img src = "./pingutype.png" id="logo" alt="PinguType Logo" />
+        <img src = "../public/pingutype.png" id="logo" alt="PinguType Logo" />
         <div className="title">
             pingutype
           <p className="subtext">a typing tool for penguins.</p>
@@ -89,9 +88,9 @@ export default function Home() {
       <div className="options">
         <button id="puncy" className="punctuation" onClick={() => toggleMenu(0)}> Punctuation </button>
         <button class = "center" id="numby" className="numbers" onClick={() => toggleMenu(1)}> Numbers </button>
-        <button id="timey15" className="time15" onClick={() => toggleMenu(3)}> 15s </button>
-        <button id="timey30" className="time30" onClick={() => toggleMenu(2)}> 30s </button>
-        <button id="timey60" className="time60" onClick={() => toggleMenu(4)}> 60s</button>
+        <button id="timey15" className="time15" onClick={() => {toggleMenu(3); setButtonTime(15)}}> 15s </button>
+        <button id="timey30" className="time30" onClick={() => {toggleMenu(2); setButtonTime(30)}}> 30s </button>
+        <button id="timey60" className="time60" onClick={() => {toggleMenu(4); setButtonTime(60)}}> 60s</button>
       </div>
       <div id="typing-test">
         <label htmlFor="typing-area">
@@ -100,7 +99,7 @@ export default function Home() {
         <div id="timerDisplay">
           <h3 id="time">Start typing to start the timer</h3>
         </div>
-        <textarea id="typing-area" rows="17" cols="120" onInput={() => startTimerOnce()}></textarea>
+        <textarea id="typing-area" rows="17" cols="120" onInput={() => startTimer()}></textarea>
       </div>
     </div>
   );
