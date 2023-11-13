862d94d9938c0bd76711bd27d6d7a2333977b87a
andhongrl <klutzjammiN@gmail.com>
2023-11-13 15:07:40 -0800

    fixed button clicking
    Made it so that the option buttons now use states and also made it so that one option must always be selected.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,6 +7,9 @@ import "../App.css"
 function Home() {
   const [timerStarted, setTimerStarted] = useState(false);
   const [endTime, setEndTime] = useState(30);
+  const [time15enabled, setTime15enabled] = useState(false);
+  const [time30enabled, setTime30enabled] = useState(true);
+  const [time60enabled, setTime60enabled] = useState(false);
   function toggleMenu(x) {
     var element; var e2; var e3;
     switch (x) {
@@ -19,40 +22,55 @@ function Home() {
         element.classList.toggle("numbers-clicked");
         return;
       case 2:
-        element = document.getElementById("timey30");
-        element.classList.toggle("time30-clicked")
-        var e2 = document.getElementById("timey15");
-        var e3 = document.getElementById("timey60");
-        if (e2.className !== "time15") {
-          e2.classList.toggle("time-clicked");
-        } if (e3.className !== "time60") {
-          e3.classList.toggle("time-clicked")
+        if (time30enabled === false) {
+          element = document.getElementById("timey30");
+          element.classList.toggle("time30-clicked")
+          var e2 = document.getElementById("timey15");
+          var e3 = document.getElementById("timey60");
+          if (e2.className !== "time15") {
+            e2.classList.toggle("time-clicked");
+            setTime15enabled(!time15enabled);
+          } if (e3.className !== "time60") {
+            e3.classList.toggle("time-clicked");
+            setTime60enabled(!time60enabled);
+          }
+          setTime30enabled(!time30enabled);
         }
         return;
       case 3:
-        element = document.getElementById("timey15");
-        element.classList.toggle("time-clicked")
-        e2 = document.getElementById("timey30");
-        e3 = document.getElementById("timey60");
-        if (e2.className == "time30") {
-          e2.classList.toggle("time30-clicked");
-        } if (e3.className !== "time60") {
-          e3.classList.toggle("time-clicked")
+        if (time15enabled === false) {
+          element = document.getElementById("timey15");
+          element.classList.toggle("time-clicked")
+          e2 = document.getElementById("timey30");
+          e3 = document.getElementById("timey60");
+          if (e2.className === "time30") {
+            e2.classList.toggle("time30-clicked");
+            setTime30enabled(!time30enabled);
+          } if (e3.className !== "time60") {
+            e3.classList.toggle("time-clicked");
+            setTime60enabled(!time60enabled);
+          }
+          setTime15enabled(!time15enabled);
         }
         return;
       case 4:
-        element = document.getElementById("timey60");
-        element.classList.toggle("time-clicked")
-        e2 = document.getElementById("timey30");
-        e3 = document.getElementById("timey15");
-        if (e2.className == "time30") {
-          e2.classList.toggle("time30-clicked");
-        } if (e3.className !== "time15") {
-          e3.classList.toggle("time-clicked")
+        if (time60enabled === false) {
+          element = document.getElementById("timey60");
+          element.classList.toggle("time-clicked")
+          e2 = document.getElementById("timey30");
+          e3 = document.getElementById("timey15");
+          if (e2.className === "time30") {
+            e2.classList.toggle("time30-clicked");
+            setTime30enabled(!time30enabled);
+          } if (e3.className !== "time15") {
+            e3.classList.toggle("time-clicked");
+            setTime15enabled(!time15enabled);
+          }
+          setTime60enabled(!time60enabled);
         }
         return;
     }
-  }  
+  }
   function startTimer() {
     if (!timerStarted)
     {
