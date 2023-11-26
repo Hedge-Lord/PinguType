057e8ef3e1221be5e9d808c3c6495cc07af46620
Andrew Hong <klutzjammin@gmail.com>
2023-11-25 20:55:48 -0800

    Made raggedy backend starter thing that basically does nothing right now

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+
+const users = [];
+app.post("/api/register", (req, res) => {
+    const { username, password, typingSpeed } = req.body;
+    const userExists = users.some(user => user.username === username);
+    if (userExists) {
+        return res.status(400).json({ error: "Username already exists" });
+    }
+    const newUser = { username, password, typingSpeed };
+    users.concat(newUser);
+    res.json({ message: "Account successfully made", user: newUser });
+});
+app.get("/api/users", (req, res) => {
+    res.json({ users });
+});
+  
+app.listen(4000, () => {
+    console.log(`Server started on port 4000`);
+});
